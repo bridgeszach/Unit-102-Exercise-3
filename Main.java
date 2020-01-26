e82df689d6e83ce3ebfd23bdb87e39bc7c87aadb
Zach Bridges <56745560+bridgeszach@users.noreply.github.com>
2020-01-25 23:07:01 -0500

    Add files via upload
    This doesn't work in the team treehouse challenge, but works in VS code.

---
+++ Main.java
@@ -0,0 +1,11 @@
+  public class Main {
+
+    public static void main(String[] args) {
+          
+      
+      Cow cow = new Cow("Larry");
+      
+      
+      System.out.printf("My cow is named %s%n", cow.getName());
+    }
+  }
